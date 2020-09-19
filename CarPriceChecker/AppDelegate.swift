742d6112671925b58e5a0d2e96928bf22bb68782
abrahamshenghur <ashenghur@gmail.com>
2020-09-19 09:57:44 -0700

    Create UITabBarController and UINavigationControllers for app foundation

--- CarPriceChecker/AppDelegate.swift
+++ CarPriceChecker/AppDelegate.swift
@@ -17,14 +17,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let searchNC = UINavigationController(rootViewController: SearchVC())
+        let savedVehiclesNC = UINavigationController(rootViewController: SavedVehiclesVC())
+
+        let tabBar = UITabBarController()
+        tabBar.viewControllers = [searchNC, savedVehiclesNC]
+        
         window = UIWindow(frame: UIScreen.main.bounds)
-        let homeViewController = ViewController()
-        homeViewController.view.backgroundColor = UIColor.red
-        window?.rootViewController = homeViewController
+        window?.rootViewController = tabBar
         window?.makeKeyAndVisible()
         
         return true
     }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ CarPriceChecker/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  CarPriceChecker
+//
+//  Created by Abraham on 9/18/20.
+//  Copyright © 2020 Abraham Shenghur. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blue
+    }
+
+}
